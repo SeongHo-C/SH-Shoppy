ca79ff02ef8cc173728c21d9ca519e34738f05f0
SeongHo-C <leeseong7130@gmail.com>
2023-08-09 20:27:21 +0900

    Feat: 로그인, 로그아웃 기능 추가

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
-import { login } from '../api/firebase';
+import { login, logout, onUserStateChange } from '../api/firebase';
 
 export default function Navbar() {
+  const [user, setUser] = useState();
+
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
+
+  useEffect(() => {
+    onUserStateChange((user) => setUser(user));
+  }, []);
+
   return (
     <header className='flex justify-between items-center p-2 border-b border-gray-300'>
       <Link className='flex items-center text-4xl text-brand' to='/'>
@@ -17,7 +31,8 @@ export default function Navbar() {
         <Link className='text-xl' to='/products/new'>
           <BsFillPencilFill />
         </Link>
-        <button onClick={login}>Login</button>
+        {!user && <button onClick={handleLogin}>Login</button>}
+        {user && <button onClick={handleLogout}>Logout</button>}
       </nav>
     </header>
   );
