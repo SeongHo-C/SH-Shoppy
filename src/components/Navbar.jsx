a9cef36ed9307bef8e8b5724975897d3d9fbdd7e
SeongHo-C <leeseong7130@gmail.com>
2023-08-17 21:06:41 +0900

    Refactor: 사용자 정보 context에서 관리

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
-import { login, logout, onUserStateChange } from '../api/firebase';
 import User from './User';
+import Button from './ui/Button';
+import { useAuthContext } from './context/AuthContext';
 
 export default function Navbar() {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    onUserStateChange(setUser);
-  }, []);
+  const { user, login, logout } = useAuthContext();
 
   return (
     <header className='flex justify-between items-center p-2 border-b border-gray-300'>
@@ -20,13 +17,15 @@ export default function Navbar() {
       </Link>
       <nav className='flex items-center gap-4 font-semibold'>
         <Link to='/products'>Products</Link>
-        <Link to='/carts'>Carts</Link>
-        <Link className='text-xl' to='/products/new'>
-          <BsFillPencilFill />
-        </Link>
+        {user && <Link to='/carts'>Carts</Link>}
+        {user?.isAdmin && (
+          <Link className='text-xl' to='/products/new'>
+            <BsFillPencilFill />
+          </Link>
+        )}
         {user && <User user={user} />}
-        {!user && <button onClick={login}>Login</button>}
-        {user && <button onClick={logout}>Logout</button>}
+        {!user && <Button text={'Login'} onClick={login} />}
+        {user && <Button text={'Logout'} onClick={logout} />}
       </nav>
     </header>
   );
